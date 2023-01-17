ac61844e2889565bcd8bbd114fe9d3601793419d
Justin-Kuchmy <53488185+Justin-Kuchmy@users.noreply.github.com>
2023-01-17 11:12:14 -0500

    Set up Junit and AsserJ for Testing

---
+++ src/test/java/com/portfolioprojects/BikeShop/BikeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioprojects.BikeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
